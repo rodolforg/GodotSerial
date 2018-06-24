5339bf2348874ccd149c0f42755994e61fe9b3b3
Rodolfo Ribeiro Gomes <rodolforg@gmail.com>
2018-06-24 11:19:27 -0300

    update demo
    Signed-off-by: Rodolfo Ribeiro Gomes <rodolforg@gmail.com>

--- demo/Node.gd
+++ demo/Node.gd
@@ -8,3 +8,17 @@ func _ready():
 	print(Serial.is_connected())
 	print(Serial.close())
 	print(Serial.is_connected())
+	
+	print(Serial.open())
+	print(Serial.available())
+	print(Serial.available_for_write())
+	print(Serial.write("oi"))
+	print(Serial.available())
+	print(Serial.available_for_write())
+	print(char(Serial.peek()))
+	print(Serial.read_string())
+	print(Serial.available())
+	print(Serial.available_for_write())
+	print(Serial.close())
+	
+	print(preload("res://addons/serial/libserial.gdns").new().peek())
